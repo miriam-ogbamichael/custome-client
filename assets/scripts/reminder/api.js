e7fa6f61af98461d8b94e79c4b2ab9e653cc911e
miriam-ogbamichael <miriamogbamichael7@gmail.com>
2020-11-10 21:18:44 -0600

    Reminders changes

--- assets/scripts/reminder/api.js
+++ assets/scripts/reminder/api.js
@@ -1,6 +1,8 @@
 const config = require('./../config')
 const store = require('./../store')
 
+// CREATE
+// Lets user create a Reminder
 const create = function (data) {
   console.log('data: ', data)
   return $.ajax({
@@ -13,10 +15,63 @@ const create = function (data) {
   })
 }
 
+// INDEX
+// Lets user access all of their Reminders
+const index = function (reminder) {
+  console.log('reminder: ', reminder)
+  return $.ajax({
+    url: config.apiUrl + 'reminders/' + reminder.id,
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    }
+  })
+}
+
+// SHOW
+// Lets user see one of their reminders
+const show = function (reminder) {
+  console.log('reminder: ', reminder)
+  return $.ajax({
+    url: config.apiUrl + '/reminders' + reminder.id,
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    }
+  })
+}
+
+// UPDATE
+// Lets user update any of their Reminders
+const update = function (data) {
+  console.log('data: ', data)
+  return $.ajax({
+    url: config.apiUrl + '/reminders/' + data.reminder.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    },
+    data: data
+  })
+}
+
+// DESTROY
+// Lets user delete any of thier Reminders
+const destroy = function (id) {
+  console.log('id: ', id)
+  return $.ajax({
+    url: config.apiUrl + '/reminders/' + id,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    }
+  })
+}
+
 module.exports = {
-  create
-  // index,
-  // show,
-  // update,
-  // destroy
+  create,
+  index,
+  show,
+  update,
+  destroy
 }
--- curl-scripts/reminders/destroy.sh
+++ curl-scripts/reminders/destroy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#!/ DESTROY request
-#!/ DELETE: deletes a specific 'id''s reminders
-
 curl "http://localhost:4741/reminders/${ID}" \
   --include \
   --request DELETE \
